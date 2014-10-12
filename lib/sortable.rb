46f6d080b663f158f538f5fb3aed9c996c7a0985
Javier Soto <sotoseattle@gmail.com>
2014-10-12 16:17:03 -0700

    refactor insert sort to avoid recursion

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -1,16 +1,13 @@
 # assumptions: 1) sort in ascending order, 2) admit repeated elements
 module Sortable
-  def insert_in_right_place(arr, e)
-    arr.each_with_index do |x, i|
-      return arr.insert(i, e) if e <= x
-    end
-    arr << e
-  end
-
   def insert_sort(arr)
-    sorted = [arr.shift]
+    sorted = []
     arr.each do |e|
-      sorted = insert_in_right_place(sorted, e)
+      if (i = sorted.find_index { |x| e < x })
+        sorted.insert(i, e)
+      else
+        sorted << e
+      end
     end
     sorted
   end
