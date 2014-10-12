309f8887886ecbe1bbadf526d8a07a59025ebc10
Javier Soto <sotoseattle@gmail.com>
2014-10-12 16:23:47 -0700

    metaprogram monkeypatch to Array with refine

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -1,14 +1,16 @@
 # assumptions: 1) sort in ascending order, 2) admit repeated elements
 module Sortable
-  def insert_sort(arr)
-    sorted = []
-    arr.each do |e|
-      if (i = sorted.find_index { |x| e < x })
-        sorted.insert(i, e)
-      else
-        sorted << e
+  refine Array do
+    def insert_sort
+      sorted = []
+      self.each do |e|
+        if (i = sorted.find_index { |x| e < x })
+          sorted.insert(i, e)
+        else
+          sorted << e
+        end
       end
+      sorted
     end
-    sorted
   end
 end
