f7b4e8891092e6cce560488645b087de8f6e16f9
Javier Soto <sotoseattle@gmail.com>
2014-10-14 06:10:01 -0700

    refactor tests

--- spec/sortable_spec.rb
+++ spec/sortable_spec.rb
@@ -5,32 +5,16 @@ describe 'Array#insert_sort algorithm' do
   let(:input) { [*1..1000] }
   let(:sorted) { [*1..1000] }
 
-  describe 'sorts arrays' do
-    it 'sorts it out' do
-      input.shuffle.insert_sort.must_equal sorted
-    end
-
-    it 'worst case scenario: already sorted' do
-      input.insert_sort.must_equal sorted
-    end
-
-    it 'best case scenario: reversed' do
-      input.reverse.insert_sort.must_equal sorted
-    end
+  describe 'sorts arrays: ordered, suffled, reversed' do
+    it { input.insert_sort.must_equal sorted }
+    it { input.shuffle.insert_sort.must_equal sorted }
+    it { input.reverse.insert_sort.must_equal sorted }
   end
 
   describe 'fringe cases' do
-    it 'single element' do
-      [1].insert_sort.must_equal [1]
-    end
-
-    it 'single repeated element' do
-      [1, 1, 1, 1].insert_sort.must_equal [1, 1, 1, 1]
-    end
-
-    it 'random repeated element' do
-      [7, 5, 5, 5, 5, 1, 2, 8].insert_sort.must_equal [1, 2, 5, 5, 5, 5, 7, 8]
-    end
+    it { [1].insert_sort.must_equal [1] }
+    it { [1, 1, 1, 1].insert_sort.must_equal [1, 1, 1, 1] }
+    it { [7, 5, 5, 5, 5, 1, 2].insert_sort.must_equal [1, 2, 5, 5, 5, 5, 7] }
   end
 
   describe 'insert_sort benchmarking' do
