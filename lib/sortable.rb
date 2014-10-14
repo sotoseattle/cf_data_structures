6a314b5f3530b934333185fdea2f3ddde6a3c587
Javier Soto <sotoseattle@gmail.com>
2014-10-14 06:02:21 -0700

    apply Derreck Maffet catch for making it stable!

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -2,7 +2,7 @@ module Sortable
   refine Array do
     def insert_sort
       reduce([]) do |sorted, e|
-        i = sorted.rindex { |x| x < e } || -1
+        i = sorted.rindex { |x| x <= e } || -1
         sorted.insert(i + 1, e)
       end
     end
