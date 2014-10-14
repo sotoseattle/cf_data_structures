e5eeba864dd910a3712fcfbe3eb4874efe18c85f
Javier Soto <sotoseattle@gmail.com>
2014-10-14 12:56:45 -0700

    basic merge sort draft

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -6,5 +6,33 @@ module Sortable
         acc.insert(i + 1, ele)
       end
     end
+
+    def conquer(a, b)
+      return a unless b
+      sol = []
+      i, j = 0, 0
+      while a[i] && b[j]
+        if a[i] <= b[j]
+          sol << a[i]
+          i += 1
+        else
+          sol << b[j]
+          j += 1
+        end
+      end
+      sol.concat(b[j..-1]).concat(a[i..-1])
+    end
+
+    def iteration(arr)
+      arr.each_slice(2).map { |a, b| conquer(a, b) }
+    end
+
+    def merge_sort
+      wip = each_slice(1).to_a
+      while wip.size > 1
+        wip = iteration(wip)
+      end
+      wip.flatten
+    end
   end
 end
--- spec/sortable_spec.rb
+++ spec/sortable_spec.rb
@@ -1,35 +1,75 @@
 require 'spec_helper'
 using Sortable
 
-describe 'Array#insert_sort algorithm' do
-  let(:input) { [*1..1000] }
-  let(:sorted) { [*1..1000] }
+describe 'Sortable' do
+  let(:input)  { [*1..1_000] }
+  let(:sorted) { [*1..1_000] }
 
-  describe 'sorts arrays: ordered, suffled, reversed' do
-    it { input.insert_sort.must_equal sorted }
-    it { input.shuffle.insert_sort.must_equal sorted }
-    it { input.reverse.insert_sort.must_equal sorted }
+  describe 'Array#insert_sort algorithm' do
+    describe 'sorts arrays: ordered, suffled, reversed' do
+      it { input.insert_sort.must_equal sorted }
+      it { input.shuffle.insert_sort.must_equal sorted }
+      it { input.reverse.insert_sort.must_equal sorted }
+    end
+
+    describe 'fringe cases' do
+      it { [1].insert_sort.must_equal [1] }
+      it { [1, 1, 1, 1].insert_sort.must_equal [1, 1, 1, 1] }
+      it { [7, 5, 5, 5, 5, 1, 2].insert_sort.must_equal [1, 2, 5, 5, 5, 5, 7] }
+    end
+
+    describe 'insert_sort benchmarking' do
+      require 'minitest/benchmark'
+      if ENV['BENCH']
+        bench_performance_constant 'insert_sort FAST', 0.9999 do |_n|
+          100.times { input.insert_sort }
+        end
+
+        bench_performance_constant 'insert_sort RANDOM', 0.9999 do |_n|
+          100.times { input.shuffle.insert_sort }
+        end
+
+        bench_performance_constant 'insert_sort SLOW', 0.9999 do |_n|
+          100.times { input.reverse.insert_sort }
+        end
+      end
+    end
   end
 
-  describe 'fringe cases' do
-    it { [1].insert_sort.must_equal [1] }
-    it { [1, 1, 1, 1].insert_sort.must_equal [1, 1, 1, 1] }
-    it { [7, 5, 5, 5, 5, 1, 2].insert_sort.must_equal [1, 2, 5, 5, 5, 5, 7] }
-  end
-
-  describe 'insert_sort benchmarking' do
-    require 'minitest/benchmark'
-    if ENV['BENCH']
-      bench_performance_constant 'insert_sort FAST', 0.9999 do |_n|
-        100.times { input.insert_sort }
+  describe 'Array#merge_sort algorithm' do
+    describe 'sorts arrays: ordered, suffled, reversed' do
+      it 'conquer!!!!' do
+        [].conquer([1,2,3], [4,5,6]).must_equal [1,2,3,4,5,6]
+        [].conquer([4,5,6], [1,2,3]).must_equal [1,2,3,4,5,6]
       end
+    end
 
-      bench_performance_constant 'insert_sort RANDOM', 0.9999 do |_n|
-        100.times { input.shuffle.insert_sort }
-      end
+    describe 'sorts arrays: ordered, suffled, reversed' do
+      it { input.merge_sort.must_equal sorted }
+      it { input.shuffle.merge_sort.must_equal sorted }
+      it { input.reverse.merge_sort.must_equal sorted }
+    end
 
-      bench_performance_constant 'insert_sort SLOW', 0.9999 do |_n|
-        100.times { input.reverse.insert_sort }
+    describe 'fringe cases' do
+      it { [1].merge_sort.must_equal [1] }
+      it { [1, 1, 1, 1, 1].merge_sort.must_equal [1, 1, 1, 1, 1] }
+      it { [7, 5, 5, 5, 5, 1, 2].merge_sort.must_equal [1, 2, 5, 5, 5, 5, 7] }
+    end
+
+    describe 'merge_sort benchmarking' do
+      require 'minitest/benchmark'
+      if ENV['BENCH']
+        bench_performance_constant 'merge_sort FAST', 0.9999 do |_n|
+          100.times { input.merge_sort }
+        end
+
+        bench_performance_constant 'merge_sort RANDOM', 0.9999 do |_n|
+          100.times { input.shuffle.merge_sort }
+        end
+
+        bench_performance_constant 'merge_sort SLOW', 0.9999 do |_n|
+          100.times { input.reverse.merge_sort }
+        end
       end
     end
   end
