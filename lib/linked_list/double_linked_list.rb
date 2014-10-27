68b3b0c1b7a069695a94220872ac086c96afdae9
Javier Soto <sotoseattle@gmail.com>
2014-10-27 15:11:36 -0700

    refactor

--- lib/linked_list/double_linked_list.rb
+++ lib/linked_list/double_linked_list.rb
@@ -20,19 +20,26 @@ class DoubleLinkedList < LightLinkedList
   end
 
   def remove(node)
-    if node == @head
-      @head = node.nexxt
-      @head.prev = nil if @head
-      return node.detach.val
-    end
-    n = @head
+    return decapitate(node) if node == head
+    n = head
     while n
-      if n == node
-        n.nexxt.prev = n.prev if n.nexxt
-        n.prev.nexxt = n.nexxt
-        return node.detach.val
-      end
+      bridge(node) if n == node
       n = n.nexxt
     end
+  end
+
+  private
+
+  def decapitate(node)
+    @head = node.nexxt
+    @head.prev = nil if head
+    node.detach.val
+  end
+
+  def bridge(node)
+    left, right = node.prev, node.nexxt
+    right.prev = left if right
+    left.nexxt = right
+    node.detach.val
   end
 end
